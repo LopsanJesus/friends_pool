ecc78c6c8327e16073e8903c386b636bb7adc202
Jesus L <lopsanjesus@gmail.com>
2023-12-20 03:41:16 +0100

    Test api hooks

--- src/pages/Matches/index.tsx
+++ src/pages/Matches/index.tsx
@@ -1,6 +1,28 @@
+import useMatches from "../../api/useMatches";
+
 import "./style.scss";
 
 const Matches = () => {
+  const { matches, loading, error } = useMatches();
+
+  if (loading) {
+    return (
+      <div className="Matches">
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="Matches">
+        <h2>Error: {error}</h2>
+      </div>
+    );
+  }
+
+  console.log(matches);
+
   return (
     <div className="Matches">
       <h2>Matches Page</h2>
--- src/pages/Players/index.tsx
+++ src/pages/Players/index.tsx
@@ -1,6 +1,28 @@
+import usePlayers from "../../api/usePlayers";
+
 import "./style.scss";
 
 const Players = () => {
+  const { players, loading, error } = usePlayers();
+
+  if (loading) {
+    return (
+      <div className="Players">
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="Players">
+        <h2>Error: {error}</h2>
+      </div>
+    );
+  }
+
+  console.log(players);
+
   return (
     <div className="Players">
       <h2>Players Page</h2>
