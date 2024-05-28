61628334fb8745bd02829167cf46d33e287767f0
Jesus L <lopsanjesus@gmail.com>
2024-05-28 21:34:05 +0200

    Create PageWithTopbar

---
+++ src/components/PageWithTopbar/index.tsx
@@ -0,0 +1,23 @@
+import TopBar from "components/TopBar";
+
+import "./style.scss";
+
+interface IProps {
+  className: string;
+  children: React.ReactNode;
+  title?: string;
+}
+
+const PageWithTopbar = ({ className, children, title }: IProps) => {
+  return (
+    <div className={`PageWithTopbar ${className}`}>
+      <TopBar />
+      <div className="container">
+        {title && <div className="title">{title}</div>}
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default PageWithTopbar;
