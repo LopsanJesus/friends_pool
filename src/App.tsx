3b05fdbd47ff8a56c13f16444075b7b327d52181
Jesus L <lopsanjesus@gmail.com>
2024-05-28 21:52:48 +0200

    Add new routes and provider to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
+
+import ErrorPage from "pages/ErrorPage";
 import Home from "./pages/Home";
-import Ranking from "./pages/Ranking";
+import Login from "./pages/Login";
 import Matches from "./pages/Matches";
 import Players from "./pages/Players";
 import Predictions from "./pages/Predictions";
+import Ranking from "./pages/Ranking";
 import Rules from "./pages/Rules";
+
+import { UserProvider } from "contexts/UserContext";
 
 import "./App.scss";
 
 const App = () => {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/ranking" element={<Ranking />} />
-          <Route path="/matches" element={<Matches />} />
-          <Route path="/players" element={<Players />} />
-          <Route path="/predictions" element={<Predictions />} />
-          <Route path="/rules" element={<Rules />} />
-        </Routes>
-      </Router>
+      <UserProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/ranking" element={<Ranking />} />
+            <Route path="/matches" element={<Matches />} />
+            <Route path="/players" element={<Players />} />
+            <Route path="/predictions" element={<Predictions />} />
+            <Route path="/rules" element={<Rules />} />
+            <Route path="/login/:token" element={<Login />} />
+            <Route path="*" element={<Navigate to="/" />} />
+            <Route path="/error" element={<ErrorPage />} />
+          </Routes>
+        </Router>
+      </UserProvider>
     </div>
   );
 };
