1c12198942328f748464f14ffd339967ca78d718
Jesus L <lopsanjesus@gmail.com>
2024-05-28 21:51:41 +0200

    New Rules page

--- src/pages/Rules/index.tsx
+++ src/pages/Rules/index.tsx
@@ -1,51 +1,21 @@
-import useInsert from "../../api/useInsert";
-
-import Button from "../../components/Button";
+import PageWithTopbar from "components/PageWithTopbar";
 
 import "./style.scss";
 
 const Rules = () => {
-  const { insert, loading, error, inserted } = useInsert({
-    databaseName: "Bets",
-  });
-
-  if (error) {
-    return (
-      <div className="Rules">
-        <h2>Error inserting bets</h2>
-      </div>
-    );
-  }
-
   return (
-    <div className="Rules">
-      <h2>Rules Page</h2>
-      <button
-        onClick={() =>
-          insert([
-            {
-              fields: {
-                Player_FK: ["rec6StpYNZr5fYi6p"],
-                Match_FK: ["recIKvxScd5qPSZlW"],
-                BetType: "FG",
-                BetValue: "3-1",
-              },
-            },
-            {
-              fields: {
-                Player_FK: ["recHMLGOOs5lIpOyo"],
-                Match_FK: ["recIKvxScd5qPSZlW"],
-                BetType: "FG",
-                BetValue: "2-2",
-              },
-            },
-          ])
-        }
-      >
-        {loading ? "Loading..." : inserted ? "Inserted" : "Insert"}
-      </button>
-      <Button text="Go back" linkTo={"/"} variant="secondary" />
-    </div>
+    <PageWithTopbar className="Rules" title="Reglas del Torneo">
+      <div className="point-rules">
+        <ul>
+          <li>Por acertar el resultado 1X2 se suma 1 punto.</li>
+          <li>Por acertar el resultado exacto se suman 3 puntos.</li>
+        </ul>
+      </div>
+
+      <div className="deadline">
+        <p>El miércoles 12 de junio a las 23:59 se cierran las predicciones.</p>
+      </div>
+    </PageWithTopbar>
   );
 };
 
