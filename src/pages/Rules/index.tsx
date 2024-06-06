18c46625d1daaaa1f28e6cfe6045ca80fa2fa906
Jesus L <lopsanjesus@gmail.com>
2024-06-06 02:42:09 +0200

    Add new Rules

--- src/pages/Rules/index.tsx
+++ src/pages/Rules/index.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from "react-i18next";
 
+import LinkButton from "components/LinkButton";
 import PageWithTopbar from "components/PageWithTopbar";
 
 import "./style.scss";
@@ -13,12 +14,30 @@ const Rules = () => {
         <p>{t("rules.deadline")}</p>
       </div>
 
-      <div className="point-rules">
+      <div className="match-predictions-rules">
+        <p>{t("rules.matchPredictions")}</p>
         <ul>
           <li>{t("rules.onePoint")}</li>
           <li>{t("rules.threePoints")}</li>
+          <li>{t("rules.keyBet")}</li>
+          <li>{t("rules.maxKeyBetsGroupPhase")}</li>
+          <li>{t("rules.maxKeyBetsFinalPhase")}</li>
         </ul>
       </div>
+
+      <div className="final-prediction-rules">
+        <p>{t("rules.finalPredictions")}</p>
+        <ul>
+          <li>{t("rules.points.groupPoolWinner")}</li>
+          <li>{t("rules.points.semifinalist1")}</li>
+          <li>{t("rules.points.semifinalist2")}</li>
+          <li>{t("rules.points.topScorer")}</li>
+          <li>{t("rules.points.finalist")}</li>
+          <li>{t("rules.points.winner")}</li>
+        </ul>
+      </div>
+
+      <LinkButton linkTo="/" text={t("buttons.back")} variant="secondary" />
     </PageWithTopbar>
   );
 };
