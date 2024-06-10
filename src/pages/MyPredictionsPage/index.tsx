6a536318b9ccee75e7a3be59a06b25bb31acb72e
Jesus L <lopsanjesus@gmail.com>
2024-06-10 13:33:57 +0200

    Add buttons to MyPredictionsPage

--- src/pages/MyPredictionsPage/index.tsx
+++ src/pages/MyPredictionsPage/index.tsx
@@ -1,10 +1,15 @@
-import useGetView from "api/useGetView";
-import Loader from "components/Loader";
-import PageWithTopbar from "components/PageWithTopbar";
-import useUser from "hooks/useUser";
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Navigate } from "react-router-dom";
+
+import useGetView from "api/useGetView";
+import useUser from "hooks/useUser";
+
+import LinkButton from "components/LinkButton";
+import Loader from "components/Loader";
+import PageWithTopbar from "components/PageWithTopbar";
+import Separator from "components/Separator";
+
 import { BetType, FinalBetType } from "types/types";
 
 const MyPredictionsPage = () => {
@@ -51,20 +56,35 @@ const MyPredictionsPage = () => {
         <>
           {dataFinalBets.map((finalBet) => (
             <div key={finalBet.id}>
-              <h3>
-                {finalBet.betType} - {finalBet.betValue}
-              </h3>
+              <div>
+                {t("predictions.betType." + finalBet.betType)} -{" "}
+                {finalBet.betValue}
+              </div>
             </div>
           ))}
+
+          <Separator />
 
           {dataBets.map((bet) => (
             <div key={bet.id}>
               <p>
-                {bet.localTeam} vs {bet.visitorTeam} {bet.localScore} -{" "}
-                {bet.visitorScore}
+                {bet.isKeyBet ? "★" : ""} {bet.localTeam} vs {bet.visitorTeam}{" "}
+                {bet.localScore} - {bet.visitorScore}
               </p>
             </div>
           ))}
+
+          {dataBets.length === 0 && (
+            <LinkButton linkTo="/predictions/matches" text={t("home.start")} />
+          )}
+
+          <Separator />
+
+          <LinkButton
+            linkTo="/predictions"
+            text={t("buttons.back")}
+            variant="secondary"
+          />
         </>
       )}
     </PageWithTopbar>
