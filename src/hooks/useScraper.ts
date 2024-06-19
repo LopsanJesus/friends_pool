1cafa8d9831b994865b9ef6a09017f1106d73dff
Jesus L <lopsanjesus@gmail.com>
2024-06-19 21:38:33 +0200

    Add condition to not scrape in local env

--- src/hooks/useScraper.ts
+++ src/hooks/useScraper.ts
@@ -1,39 +1,42 @@
 import axios from "axios";
+import { localScrapping } from "config/constants";
 import { useEffect, useState } from "react";
 
 const useScraper = (url: string, interval: number) => {
-  const [loading, setLoading] = useState<boolean>(true);
+  const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
   const [fetchedData, setFetchedData] = useState<boolean>(false);
 
   useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      setFetchedData(false);
+    if (localScrapping) {
+      const fetchData = async () => {
+        setLoading(true);
+        setFetchedData(false);
 
-      try {
-        const response = await axios.get(url);
-        console.log("Fetched data:", response.data);
-        setFetchedData(true);
-      } catch (error) {
-        if (axios.isAxiosError(error)) {
-          setError(error.message);
-        } else {
-          setError("An unexpected error occurred");
+        try {
+          const response = await axios.get(url);
+          console.log("Fetched data:", response.data);
+          setFetchedData(true);
+        } catch (error) {
+          if (axios.isAxiosError(error)) {
+            setError(error.message);
+          } else {
+            setError("An unexpected error occurred");
+          }
+        } finally {
+          setLoading(false);
         }
-      } finally {
-        setLoading(false);
-      }
-    };
+      };
 
-    // Initial fetch when component mounts
-    fetchData();
+      // Initial fetch when component mounts
+      fetchData();
 
-    // Interval to fetch data periodically
-    const intervalId = setInterval(fetchData, interval);
+      // Interval to fetch data periodically
+      const intervalId = setInterval(fetchData, interval);
 
-    // Clean up interval on unmount
-    return () => clearInterval(intervalId);
+      // Clean up interval on unmount
+      return () => clearInterval(intervalId);
+    }
   }, [url, interval]);
 
   return { fetchedData, loading, error };
